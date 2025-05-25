50396006ccf9add716c938b0afc5e4e14ac35691
Jose Rodriguez <89029438+LinJose02@users.noreply.github.com>
2025-05-25 00:00:11 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function setActive(element) {
-    // Selecciona todos los elementos con la clase 'nav-link' y elimina la clase 'active' de cada uno
-    document.querySelectorAll('.nav-link').forEach(link => {
-        link.classList.remove('active');
-    });
-    // Añade la clase 'active' al elemento que fue clicado
-    element.classList.add('active');
-}
